a09e92f30af18dc4092cd21916c28dbedc6ad0cc
franarci <fran.arciniega96@gmail.com>
2021-10-14 16:29:35 -0300

    Profesor

---
+++ LibreriaEscuela/Models/Profesor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaEscuela.Models
+{
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int DNI { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Titulo { get; set; }
+
+        public Profesor(string nombre, string apellido, int dni, DateTime fechaNac, string titulo)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            DNI = dni;
+            FechaNacimiento = fechaNac;
+            Titulo = titulo;
+        }
+    }
+}
