459515fd36510015376be2adba464f6855718dff
franarci <fran.arciniega96@gmail.com>
2021-10-14 16:31:16 -0300

    Comportamiento de botones

--- WindowsEscuela/Form1.cs
+++ WindowsEscuela/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibreriaEscuela.Models;
 
 namespace WindowsEscuela
 {
@@ -15,6 +16,34 @@ namespace WindowsEscuela
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnProfesor_Click(object sender, EventArgs e)
+        {
+            Profesor profesor =
+                new Profesor(
+                     Microsoft.VisualBasic.Interaction.InputBox("Ingrese un nombre"),
+                     Microsoft.VisualBasic.Interaction.InputBox("Ingrese un apellido"),
+                     Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Ingrese un DNI")),
+                     Convert.ToDateTime(Microsoft.VisualBasic.Interaction.InputBox("Ingrese una fecha de nacimiento, Ej: 05 / 01 / 1996")),
+                     Microsoft.VisualBasic.Interaction.InputBox("Ingrese un titulo")
+                     );
+
+            lstProfesores.Items.Add(profesor.Nombre + " " + profesor.Apellido + " DNI: " + profesor.DNI + " FechaNacimiento: " + Convert.ToString(profesor.FechaNacimiento) + " " + profesor.Titulo);
+        }
+
+        private void btnAlumno_Click(object sender, EventArgs e)
+        {
+            Alumno alumno =
+                new Alumno(
+                     Microsoft.VisualBasic.Interaction.InputBox("Ingrese un nombre"),
+                     Microsoft.VisualBasic.Interaction.InputBox("Ingrese un apellido"),
+                     Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Ingrese un DNI")),
+                     Convert.ToDateTime(Microsoft.VisualBasic.Interaction.InputBox("Ingrese una fecha de nacimiento, Ej: 05 / 01 / 1996")),
+                     Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Ingrese un legajo"))
+                     );
+
+            lstAlumnos.Items.Add(alumno.Nombre + " " + alumno.Apellido + " DNI: " + alumno.DNI + " FechaNacimiento: " + Convert.ToString(alumno.FechaNacimiento) + " " + alumno.NroLegajo);
         }
     }
 }
