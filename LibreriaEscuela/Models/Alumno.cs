48b73b14050f9db265513d4f22cfaedf9d145236
franarci <fran.arciniega96@gmail.com>
2021-10-14 16:28:09 -0300

    Alumno

---
+++ LibreriaEscuela/Models/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaEscuela.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int DNI { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int NroLegajo { get; set; }
+
+        public Alumno(string nombre, string apellido, int dni, DateTime fechaNac, int nroLegajo)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            DNI = dni;
+            FechaNacimiento = fechaNac;
+            NroLegajo = nroLegajo;
+        }
+    }
+}
